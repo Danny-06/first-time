639580d11c6b95ab83546db67dfa5ac1395ed918
Danny <daniel.fabian13x@gmail.com>
2023-09-28 23:30:05 +0200

    Handle inner cyclic reference in Object.print

--- utils/object.lua
+++ utils/object.lua
@@ -60,11 +60,74 @@ function Object.print(object)
   print(Object.stringify(object))
 end
 
+---Helper functions for lists
+
+---
+---@param list any[] | nil
+---@param item any
+---@return boolean, number?
+local function listContainsItem(list, item)
+  if list == nil then
+    return false
+  end
+
+  for index = 1, #list, 1 do
+    if list[index] == item then
+      return true, index
+    end
+  end
+
+  return false
+end
+
+---
+---@param list any[] | nil
+---@param item any
+---@return boolean, number?
+local function listContainsItemReverse(list, item)
+  if list == nil then
+    return false
+  end
+
+  for index = #list, 1, -1 do
+    if list[index] == item then
+      return true, index
+    end
+  end
+
+  return false
+end
+
+---
+---@param list any[]
+---@return any[]
+local function cloneList(list)
+  if list == nil then
+    error("'list' cannot be 'nil'")
+  end
+
+  local clonedList = {}
+
+  for index = 1, #list, 1 do
+    clonedList[index] = list[index]
+  end
+
+  return clonedList
+end
+
 --Transform objects into its string representation
 ---@param object table
 ---@param initalIdentation? string
+---@param parents? table[]
 ---@return string
-function Object.stringify(object, initalIdentation)
+function Object.stringify(object, initalIdentation, parents)
+  parents = parents or {}
+
+  print('Parents: '..#parents)
+  for index, value in ipairs(parents) do
+    print('- '..tostring(value))
+  end
+
   if type(object) ~= 'table' then
     return tostring(object)
   end
@@ -74,6 +137,15 @@ function Object.stringify(object, initalIdentation)
   elseif getmetatable(object) ~= nil and type(getmetatable(object).__tostring) == 'function' then
     return getmetatable(object).__tostring(object)
   end
+
+  -- Hopefully, one day I will implement key sorting
+  -- local keyTypesOrder = {
+  --   'boolean',
+  --   'number',
+  --   'string',
+  --   'table',
+  --   'function',
+  -- }
 
   local identation = '  '
 
@@ -110,12 +182,20 @@ function Object.stringify(object, initalIdentation)
 
     local stringifiedValue = value
 
+    local valueIsEqualToAncestor, ancestorIndex = listContainsItemReverse(parents, value)
+    local parentDeepLevel = valueIsEqualToAncestor and (#parents - ancestorIndex + 1) or nil
+
     if value == object then
       stringifiedValue = '[[Self]]'
+    elseif valueIsEqualToAncestor then
+      stringifiedValue = '[[Parent: '..parentDeepLevel..']]'
     elseif type(value) == 'table' then
-      stringifiedValue = Object.stringify(value, identation..'  ')
+      local clonedParents = cloneList(parents)
+      table.insert(clonedParents, object)
+
+      stringifiedValue = Object.stringify(value, identation..'  ', clonedParents)
     elseif type(value) == 'string' then
-      stringifiedValue = "'"..value.."'"
+      stringifiedValue = ("'"..value.."'")
     end
 
     objectString = objectString..identation..'['..stringifiedKey..']: '..tostring(stringifiedValue)..',\n'
@@ -123,12 +203,18 @@ function Object.stringify(object, initalIdentation)
 
   local metatable = getmetatable(object)
   if metatable ~= nil then
-    objectString = objectString..'\n'..identation.."[[MetaTable]]: "..Object.stringify(metatable, identation..'  ')..',\n'
+    local clonedParents = cloneList(parents)
+    table.insert(clonedParents, object)
+
+    objectString = objectString..'\n'..identation.."[[MetaTable]]: "..Object.stringify(metatable, identation..'  ', clonedParents)..',\n'
   end
 
   local debugMetatable = debug.getmetatable(object)
   if debugMetatable ~= nil then
-    objectString = objectString..identation.."[[Debug: MetaTable]]: "..Object.stringify(debugMetatable, identation..'  ')..',\n'
+    local clonedParents = cloneList(parents)
+    table.insert(clonedParents, object)
+
+    objectString = objectString..identation.."[[Debug: MetaTable]]: "..Object.stringify(debugMetatable, identation..'  ', clonedParents)..',\n'
   end
 
   dicreaseIndentation()
