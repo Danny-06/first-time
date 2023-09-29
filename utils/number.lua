33b323e7b7e17f29ce6cd8bda7b11fea4517d4d8
Danny <daniel.fabian13x@gmail.com>
2023-09-30 01:12:23 +0200

    Add Number.isFinite()

--- utils/number.lua
+++ utils/number.lua
@@ -7,5 +7,12 @@ function Number.isNaN(number)
   return type(number) == 'number' and number ~= number
 end
 
+---
+---@param number number
+---@return boolean
+function Number.isFinite(number)
+  return not Number.isNaN(number) and math.abs(number) ~= math.huge
+end
+
 
 return Number
