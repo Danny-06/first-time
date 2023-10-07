6b16a6adfe246ec351edc05d3888dd3aca41392e
Danny <daniel.fabian13x@gmail.com>
2023-10-07 11:17:52 +0200

    Add Number.rotateIfOutOfRange()

--- utils/number.lua
+++ utils/number.lua
@@ -3,6 +3,7 @@ local Number = {}
 ---
 ---@param number number
 ---@return boolean
+---@nodiscard
 function Number.isNaN(number)
   return type(number) == 'number' and number ~= number
 end
@@ -10,8 +11,42 @@ end
 ---
 ---@param number number
 ---@return boolean
+---@nodiscard
 function Number.isFinite(number)
   return not Number.isNaN(number) and math.abs(number) ~= math.huge
+end
+
+---
+---@param number number
+---@param from number
+---@param to number
+---@return number
+---@nodiscard
+function Number.rotateIfOutOfRange(number, from, to)
+  if from >= to then
+    error("'from' must be lower than 'to'")
+  end
+
+  local result = number
+
+  local normalizedFrom = 0 -- from - from
+  local normalizedTo = to - from
+  local normalizedNumber = number - from
+
+  if normalizedNumber > normalizedTo then
+    result = normalizedNumber % (normalizedTo + 1)
+    result = result + from
+  elseif normalizedNumber < normalizedFrom then
+    result = (normalizedTo + 1) - math.abs(normalizedNumber) % (normalizedTo + 1)
+
+    if result == normalizedTo + 1 then
+      result = 0
+    end
+
+    result = result + from
+  end
+
+  return result
 end
 
 
