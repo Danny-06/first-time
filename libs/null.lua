af7a76359e4d5ded7aba77508b4fe73d2d57bbe8
Danny <daniel.fabian13x@gmail.com>
2023-10-17 22:37:56 +0200

    Implement errors on null accessors

--- libs/null.lua
+++ libs/null.lua
@@ -3,5 +3,13 @@
 null = setmetatable({}, {
   __tostring = function ()
     return 'null'
+  end,
+
+  __index = function (self, key)
+    error("Cannot get property '"..tostring(key).."' of null")
+  end,
+
+  __newindex = function (self, key, value)
+    error("Cannot set property '"..tostring(key).."' of null to value '"..tostring(value).."'")
   end
 })
